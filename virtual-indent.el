50e93c590a57c652c2cbd41d2cecf9a345ff9f02
Eric Kaschalk <ekaschalk@gmail.com>
2019-01-14 12:41:06 -0800

    Add: Relevant components from lig-mode and pretty-fonts ported

--- virtual-indent.el
+++ virtual-indent.el
@@ -15,4 +15,57 @@
 (require 'dash-functional)
 (require 's)
 
-;;; Virtual-Indent
+;;; Configuration
+
+(defconst virtual-indent-rgx-string-alist
+  '(("\\(hello\\)" ""))
+  "Alist of rx and string replacement.")
+
+;;; Overlays
+
+(defun virtual-indent--make-ov (subexp)
+  "Create overlay with start and end taking `match-data' at SUBEXP."
+  (make-overlay (match-beginning subexp)
+                (match-end       subexp)))
+
+(defun virtual-indent--ov-mod-hook (ov post-modification? start end &optional _)
+  "Overlay modification hook to force evaporation upon modification within ov."
+  (when post-modification?
+    (-doto ov
+      (overlay-put 'display nil)
+      (overlay-put 'modification-hooks nil))))
+
+;;; Font-Locks
+
+(defun virtual-indent--build-kwd (rgx string)
+  `(,rgx (0 (prog1 nil
+              (-doto (virtual-indent--make-ov 1)
+                (overlay-put 'display string)
+                (overlay-put 'evaporate t)
+                (overlay-put 'modification-hooks '(lig-mod-hook)))
+              ;; run in the indent recalculations
+              ))))
+
+(defun virtual-indent-add-kwds (rgx-string-alist)
+  (->> rgx-string-alist
+     (-map (-applify #'rgx-string-alist))
+     (font-lock-add-keywords nil)))
+
+(defun virtual-indent-hook-fn ()
+  (virtual-indent-add-kwds virtual-indent-rgx-string-alist))
+
+(defun virtual-indent-disable ()
+  (interactive)
+  (remove-hook 'lisp-mode-hook virtual-indent-hook-fn))
+
+(defun virtual-indent-enable ()
+  (interactive)
+  (virtual-indent-disable)
+  (add-hook    'lisp-mode-hook virtual-indent-hook-fn))
+
+;;; Indentation
+
+;; `indent-sexp' - lisp-mode only
+;; `indent-region' - all-modes (start end)
+;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Truncation.html#Truncation
+;; `line-prefix' and `wrap-prefix' used by org-indent
