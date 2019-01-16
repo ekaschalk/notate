8f3fb83ddb03388afba4003d1a6b2e16176906e8
Eric Kaschalk <ekaschalk@gmail.com>
2019-01-16 12:57:26 -0800

    Add: Virtual-indent-spec tentative data structure

--- virtual-indent.el
+++ virtual-indent.el
@@ -17,9 +17,23 @@
 
 ;;; Configuration
 
-(defconst virtual-indent-rgx-string-alist
-  '(("\\(hello\\)" ""))
-  "Alist of rx and string replacement.")
+(defun virtual-indent-make-spec (name string replacement &optional rx)
+  "Create spec plist NAME for STRING to REPLACEMENT optionally with custom RX."
+  `(:name
+    ,name
+    :string      ,string
+    :rx          ,(or rx
+                      `,(rx-to-string `(group ,string)
+                                      'no-shy-group))
+    :replacement ,replacement
+    :width       ,(- (length string)
+                     (length replacement))))
+
+(defconst virtual-indent-spec
+  (list (virtual-indent-make-spec "Test Ligature" "hello" "")
+        ;; more ligatures...
+        )
+  "Collection of `virtual-indent-make-spec' specifying ligature replacements.")
 
 ;;; Overlays
 
@@ -27,6 +41,12 @@
   "Create overlay with start and end taking `match-data' at SUBEXP."
   (make-overlay (match-beginning subexp)
                 (match-end       subexp)))
+
+(defun virtual-indent--ov-in (subexp)
+  (-when-let* ((overlays (overlays-in (match-beginning subexp)
+                                      (match-end       subexp)))
+               (-contains? overlays)
+               )))
 
 (defun virtual-indent--ov-mod-hook (ov post-modification? start end &optional _)
   "Overlay modification hook to force evaporation upon modification within ov."
@@ -39,6 +59,12 @@
 
 (defun virtual-indent--build-kwd (rgx string)
   `(,rgx (0 (prog1 nil
+              ;; (print "found")
+
+              (unless (-contains?)
+                (overlays-at)
+                )
+
               (-doto (virtual-indent--make-ov 1)
                 (overlay-put 'display string)
                 (overlay-put 'evaporate t)
@@ -48,7 +74,7 @@
 
 (defun virtual-indent-add-kwds (rgx-string-alist)
   (->> rgx-string-alist
-     (-map (-applify #'rgx-string-alist))
+     (-map (-applify #'virtual-indent--build-kwd))
      (font-lock-add-keywords nil)))
 
 (defun virtual-indent-hook-fn ()
@@ -56,16 +82,13 @@
 
 (defun virtual-indent-disable ()
   (interactive)
-  (remove-hook 'lisp-mode-hook virtual-indent-hook-fn))
+  (remove-hook 'lisp-mode-hook #'virtual-indent-hook-fn))
 
 (defun virtual-indent-enable ()
   (interactive)
   (virtual-indent-disable)
-  (add-hook    'lisp-mode-hook virtual-indent-hook-fn))
+  (setq font-lock-keywords nil)
+  (add-hook    'lisp-mode-hook #'virtual-indent-hook-fn)
+  (lisp-mode))
 
 ;;; Indentation
-
-;; `indent-sexp' - lisp-mode only
-;; `indent-region' - all-modes (start end)
-;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Truncation.html#Truncation
-;; `line-prefix' and `wrap-prefix' used by org-indent
