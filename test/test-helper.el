934bffb6e4b26136dbb66f38b7c4cb2e9d781c05
Eric Kaschalk <ekaschalk@gmail.com>
2019-02-22 14:34:46 -0800

    Docs: extra commentary, missing header/lexical-bind on test-helper

--- test/test-helper.el
+++ test/test-helper.el
@@ -1,23 +1,13 @@
+;;; test-helper.el --- Testing Macros -*- lexical-binding: t -*-
+
+
+
 (require 'ert)
 (require 'faceup)
 (require 'f)
 
 (progn (add-to-list 'load-path (-> (f-this-file) (f-parent) (f-parent)))
        (require 'aplig))
-
-
-
-;;; Asserts
-
-;; Aliases for (compose `should' predicate). Not required, but I like it.
-
-(defmacro assert= (f1 f2) `(should (= ,f1 ,f2)))
-(defmacro assert/= (f1 f2) `(should (/= ,f1 ,f2)))
-(defmacro assert-eq (f1 f2) `(should (eq ,f1 ,f2)))
-(defmacro assert-neq (f1 f2) `(should (neq ,f1 ,f2)))
-(defmacro assert-s= (s1 s2) `(should (s-equals? ,s1 ,s2)))
-(defmacro assert-size (coll size) `(assert= (length ,coll) ,size))
-(defmacro assert-size= (coll1 coll2) `(should (= (length ,coll1) (length ,coll2))))
 
 
 
@@ -59,7 +49,9 @@ KIND is a symbol identifying how ligs will contribute to masks:
 
    'lispy: Ligs use lisp boundary functions to contribute to masks.
 
-   'any: Execute BODY for the following values of KIND: minimal, simple, lispy.
+   'any: Execute BODY for each of the following values of KIND:
+           minimal, simple and lispy
+
          Useful when mask-lig interaction is present but doesn't matter.
 
 After setting the context, `aplig-setup--agnostic' is executed. At the time of
@@ -74,9 +66,9 @@ writing, it instantiates empty masks for the buffer and sets up managed vars."
   ;; Collapsing into an if causes max-eval-depth err, haven't looked into why
   `(unless (eq 'any ,kind)
      (with-temp-buffer
-       (aplig-disable)
+       (aplig-disable)  ; just-in-case reset managed vars
        (aplig-test--kind->context ,kind)
-       (insert (s-trim ,buffer-contents))
+       (insert (s-trim ,buffer-contents))  ; so test lines 1-idxed not 2-idxed
        (aplig-setup--agnostic)
        ,@body
        (aplig-disable))))
@@ -100,3 +92,18 @@ writing, it instantiates empty masks for the buffer and sets up managed vars."
 (defun aplig-test--mock-ligs (string-replacement-alist)
   "Map `aplig-test--mock-lig' over list STRING-REPLACEMENT-ALIST."
   (-mapcat (-applify #'aplig-test--mock-lig) string-replacement-alist))
+
+
+
+;;; Asserts
+
+;; Aliases for `should' variations. Not required, but I like it, as it makes
+;; assertions captured in threads slightly cleaner.
+
+(defmacro assert= (f1 f2) `(should (= ,f1 ,f2)))
+(defmacro assert/= (f1 f2) `(should (/= ,f1 ,f2)))
+(defmacro assert-eq (f1 f2) `(should (eq ,f1 ,f2)))
+(defmacro assert-neq (f1 f2) `(should-not (eq ,f1 ,f2)))
+(defmacro assert-s= (s1 s2) `(should (s-equals? ,s1 ,s2)))
+(defmacro assert-size (coll size) `(assert= (length ,coll) ,size))
+(defmacro assert-size= (coll1 coll2) `(assert= (length ,coll1) (length ,coll2)))
