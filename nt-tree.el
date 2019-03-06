e7dff2f1d2fff976ac2e31589f5d263a627ebf98
Eric Kaschalk <ekaschalk@gmail.com>
2019-03-06 12:45:11 -0800

    Add: More tree querying aliases, better impls of roots finding

--- nt-tree.el
+++ nt-tree.el
@@ -1,4 +1,4 @@
-;;; nt-tree.el --- Note Nodes -*- lexical-binding: t; -*-
+;;; nt-tree.el --- Note Ov Tracking -*- lexical-binding: t; -*-
 
 ;; Copyright © 2019 Eric Kaschalk <ekaschalk@gmail.com>
 
@@ -38,7 +38,7 @@ Roots are non-overlapping line-intervals with P-C relationship defined as:
 
 ---
 
-Notes are sorted as follows:
+Notes are sorted as follows (via cmp fn `nt-tree--note<'):
 
   - n_1 < n_2 if n_1 is a child of n_2
 
@@ -48,9 +48,12 @@ So the ordering looks like:
 
   - Sort roots by buffer position.
 
-  - Insert before each root its children ordered by increasing size.")
+  - Insert before each root its children ordered by increasing size.
 
-;;; Aliases
+This ordering should be maintained for optimized(able) parent/child lookup.")
+
+;;; Querying
+;;;; Top-level
 
 (defun nt-tree->list ()
   "Return list of managed notes by `nt-tree'."
@@ -60,50 +63,61 @@ So the ordering looks like:
   "Return roots of `nt-tree'."
   (hierarchy-roots nt-tree))
 
-;;; Relationships
-;;;; Root-Finding
+;;;; Notes
+
+(defun nt-tree--contains? (note)
+  "Is NOTE contained in `nt-tree'?"
+  (hierarchy-has-item nt-tree note))
+
+(defun nt-tree--leafs (&optional note)
+  "Return the smallest-boundary notes, optionally restricted to NOTE's subtree."
+  (hierarchy-leafs nt-tree note))
+
+(defun nt-tree--note->parent (note)
+  "Return parent of NOTE, possibly being itself."
+  (hierarchy-parent nt-tree note))
+
+(defun nt-tree--note->root (note)
+  "Return root of NOTE, possibly being itself."
+  ;; Alternatively could do the natural choice of visiting parents
+  (-first (-partial #'nt-tree--note-is-subset? note)
+          (nt-tree->roots)))
+
+;;;; Regions
+
+(defun nt-tree--region->notes (start end)
+  "Return notes within region [START END)"
+  (-filter (-cut #'overlay-get <> 'nt-note)
+           (overlays-in start end)))
 
 (defun nt-tree--region->roots (start end)
   "Return roots covering region [START END)."
-  (let* ((roots
-          (nt-tree->roots))
-         (start-idx
-          (-find-index (-lambda ((a _) it)
-                         (<= start a))
-                       roots))
-         (end-idx
-          (-find-last-index (-lambda ((_ b) it)
-                              (<= b end))
-                            roots)))
-    (-slice roots
-            (or start-idx 0)
-            (or end-idx (length roots)))))
+  (-map #'nt-tree--note->root
+        (nt-tree--region->notes start end)))
 
 (defun nt-tree--point->root (pos)
   "Return root containing POS"
   (-first-item (nt-tree--region->roots pos (1+ pos))))
 
-(defun nt-tree--note->root (note)
-  "Return root of NOTE, possibly being itself."
-  (-> note overlay-start nt-tree--point->root))
+;;; Relationships
 
 ;;;; Comparisons
 
 (defun nt-tree--note-is-subset? (note-1 note-2)
-  "Is NOTE-1's boundary captured in NOTE-2's boundary?"
+  "Is NOTE-1's boundary captured in NOTE-2's boundary? Non-strict."
   (-let (((a1 b1) (nt-note->bound note-1))
          ((a2 b2) (nt-note->bound note-2)))
     (and (<= a2 a1)
          (<= b1 b2))))
 
 (defun nt-tree--note-start< (note-1 note-2)
-  "Compare NOTE-1's start and NOTE-2's start positions."
+  "Compare NOTE-1's start and NOTE-2's start positions. Non-strict."
   (-let (((a1 _) (nt-note->bound note-1))
          ((a2 _) (nt-note->bound note-2)))
     (<= a2 a1)))
 
 (defun nt-tree--note< (note-1 note-2)
-  "Compare NOTE-1 and NOTE-2."
+  "Compare NOTE-1 and NOTE-2. Non-strict."
   (cond (nt-tree--note-is-subset? note-1 note-2)
         (nt-tree--note-start<     note-1 note-2)))
 
@@ -128,6 +142,30 @@ So the ordering looks like:
 (defun nt-tree--sort ()
   "Sort `nt-tree' accordding to `nt-tree--note<' comparison fn."
   (hierarchy-sort nt-tree #'nt-tree--note<))
+
+;;; Scratch
+
+;; Manual version, possibly faster depending on hierarchy's implementation
+;; (defun nt-tree--region->roots (start end)
+;;   "Return roots covering region [START END)."
+;;   (let* ((roots
+;;           (nt-tree->roots))
+;;          (start-idx
+;;           (-find-index (-lambda ((a _) it)
+;;                          (<= start a))
+;;                        roots))
+;;          (end-idx
+;;           (-find-last-index (-lambda ((_ b) it)
+;;                               (<= b end))
+;;                             roots)))
+;;     (-slice roots
+;;             (or start-idx 0)
+;;             (or end-idx (length roots)))))
+
+;; Manual version
+;; (defun nt-tree--note->root (note)
+;;   "Return root of NOTE, possibly being itself."
+;;   (-> note overlay-start nt-tree--point->root))
 
 ;;; Provide
 
