38432ccd9406bb396fd3b7e43c1d295170c4199a
Eric Kaschalk <ekaschalk@gmail.com>
2019-04-25 12:16:50 -0700

    Docs: Lots more case work on unbalanced sexp deletion

--- test/nt-change-test.el
+++ test/nt-change-test.el
@@ -3,7 +3,7 @@
 ;; slup-unbalanced - deleting a (
 ;; barf-unbalanced - deleting a )
 
-;;; Deletion
+;;; Deletion (single-line)
 ;;;; Slurp-Unbalanced
 
 ;; CASES:
@@ -15,7 +15,89 @@
 
 ;; CHANGE: Delete the "(" under X
 ;; RESULT: Mask 5 has note removed
-;; IMPL: Extract root containing X.
-;;       Update bounds of notes contained in root up until X.
+;; IMPL:
+;; 1. Extract root containing line of deletion, if such a root exists.
+;; 2. Update bounds of notes contained in root up until X.
+;; 3. For each note captured:
+;;      Delete note in masks in [bound last-bound)
+;;      Add note in masks in [last-bound bound)
 
 ;;;; Barf-Unbalanced
+
+;; CASES:
+;; (note bar
+;;       (note foo
+;;                X
+;;             bar)
+;;       (foo note foo
+;;            foo)
+;;       bar)
+;; =>
+;; (note bar
+;;       (note foo
+;;                X
+;;             bar
+;;             (foo note foo
+;;                  foo)
+;;             bar)
+
+;; CHANGE: Delete the ")" under X
+;; RESULT: 2nd note added to masks up until final line
+;; IMPL:
+;; 1. Extract root containing line of deletion, if such a root exists.
+;; 2. From root up until smallest child s.t. bound < line of deletion:
+;;    Update bounds of notes contained in root up until X.
+;; then what...
+
+;;;; Barf-Unbalanced that eats roots
+;;;;; Hungry
+
+;; Also possible deletion makes a new root.
+;; Though, since I'm not storing roots as it is, this might not be necessary
+;; to think about in a special way.
+
+;; But, I'm using 'nt-last-bound to find the roots right. So why am I not
+;; storing the roots?
+
+;; CASE:
+;; (note foo
+;;          X
+;;       bar)
+;; (note foo
+;;       bar)
+;; (note foo
+;;       bar)
+
+;; CHANGE: Delete the ")" under X
+;; RESULT:
+;; Same as before but we are checking the first-note-after-the-root==next-root
+;; In that case - we eat the next root... and possibly more roots afterwards...
+
+;;;;; Petite
+
+;; CASE:
+
+;; CASE:
+;; (note foo
+;;          X
+;;       bar)
+;; (note foo
+;;       bar))
+;; (note foo
+;;       bar)
+
+;; CHANGE: Delete the ")" under X
+;; RESULT:
+;; We only eat the first root this time.
+
+;;; GENERAL NOTES
+
+;; In
+;; (note foo
+;;          X
+;;       bar)
+;; (note foo
+;;       bar))
+
+;; When we delete ")", the boundary checking will fail because we haven't
+;; indented that line yet!
